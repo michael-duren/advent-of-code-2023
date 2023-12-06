d282cda79cf12f1bf0861512ce69541d244c0e41
Michael Duren <michaeld@michaelduren.com>
2023-12-06 09:57:57 -0600

    add parsing logic

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Shared;
-using Shared.Solutions.DayFour;
+using Shared.Solutions.DayFive;
 
-string inputPath = PathInputFactory.Create("Four").Input;
+string inputPath = PathInputFactory.Create("Five").Test;
 
 List<string> input = Helpers.ReadInput(inputPath);
-int result = PartTwo.Solve(input);
+int result = PartOne.Solve(input);
 
 Console.WriteLine($"Result: {result}");
--- Shared/Solutions/DayFive/PartOne.cs
+++ Shared/Solutions/DayFive/PartOne.cs
@@ -1,9 +1,73 @@
 namespace Shared.Solutions.DayFive
 {
-    public class PartOne
+    public static class PartOne
     {
+        public readonly struct CategoryItem
+        {
+            public ulong DestinationRangeStart { get; init; }
+            public ulong SourceRangeStart { get; init; }
+            public ulong Range { get; init; }
+        }
+
+        public class Category
+        {
+            public string Name { get; init; } = null!;
+            public List<CategoryItem> Items { get; init; } = new();
+        }
+
+        public static (List<Category>, List<ulong>) ParseInput(List<string> lines)
+        {
+            List<Category> categories = new();
+            List<ulong> seeds = new();
+            bool inCategory = false;
+
+            foreach (string line in lines)
+            {
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    inCategory = false;
+                    continue;
+                }
+
+                if (line.Contains("seeds"))
+                    seeds = line.Split(" ").Skip(1).Select(x => ulong.TryParse(x, out ulong seed) ? seed : 0).ToList();
+
+                if (line.Contains("map"))
+                {
+                    string categoryName = line.Split(" ").First();
+                    inCategory = true;
+                    Category category = new()
+                    {
+                        Name = categoryName,
+                        Items = new List<CategoryItem>()
+                    };
+                    categories.Add(category);
+                    continue;
+                }
+
+                if (inCategory && categories.Count > 0)
+                {
+                    string[] parts = line.Split(" ");
+
+                    categories[^1].Items.Add(new CategoryItem
+                    {
+                        DestinationRangeStart = ulong.TryParse(parts[0], out ulong destinationRangeStart) ? destinationRangeStart : 0,
+                        SourceRangeStart = ulong.TryParse(parts[1], out ulong sourceRangeStart) ? sourceRangeStart : 0,
+                        Range = ulong.TryParse(parts[2], out ulong range) ? range : 0
+                    });
+                }
+
+            }
+
+
+            return (categories, seeds);
+        }
+
+
         public static int Solve(List<string> lines)
         {
+            (List<Category> categories, List<ulong> seeds) = ParseInput(lines);
             return 0;
         }
     }
