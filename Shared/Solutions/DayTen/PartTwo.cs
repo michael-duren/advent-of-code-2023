59bbb2224e262210433776587f1ffdb0e5ad5eb7
Michael Duren <michaeld@michaelduren.com>
2023-12-11 07:23:27 -0600

    setup day 10

---
+++ Shared/Solutions/DayTen/PartTwo.cs
@@ -0,0 +1,10 @@
+namespace Shared.Solutions.DayTen
+{
+    public static class PartTwo
+    {
+        public static int Solve(List<string> input)
+        {
+            return 0;
+        }
+    }
+}
